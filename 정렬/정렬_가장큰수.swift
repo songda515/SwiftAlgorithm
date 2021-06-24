f1cb20164fed737a1e57367dee10f6b3583fb1ec
Danna <songda515@gmail.com>
2021-06-25 01:35:36 +0900

    [정렬] 가장 큰 수 - 두번째 풀이 추가

--- 정렬/정렬_가장큰수.swift
+++ 정렬/정렬_가장큰수.swift
@@ -9,10 +9,10 @@ import Foundation
 
 func 정렬_가장큰수() {
     
-    func solution(_ numbers:[Int]) -> String {
+    // 첫번째 풀이 - sort, reduce, if-else 활용
+    func solution1(_ numbers:[Int]) -> String {
         
         var stringNumbers: [String] = numbers.map { String($0) }
-
         stringNumbers.sort { prev, next in
             let first = prev + next
             let second = next + prev
@@ -26,6 +26,15 @@ func 정렬_가장큰수() {
             return answer
         }
     }
+    
+    // 두번째 풀이 - sorted 축약, joined, 삼항연산자 활용
+    func solution(_ numbers:[Int]) -> String {
+
+        let stringNumbers: [String] = numbers.map { String($0) }
+        let answer = stringNumbers.sorted { return $0+$1 > $1+$0 }.joined()
+        return answer.first! == "0" ? "0" : answer
+    }
+    
     print(solution([6, 10, 2])) // 6210
     print(solution([3, 30, 34, 5, 9])) // 9534330
     print(solution([0, 0, 0, 0])) // 0
